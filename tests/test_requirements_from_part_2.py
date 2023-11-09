d9d0f6bed430a32e5299816a10c31ade63ef5221
Aaron Wolmutt <awolmutt@Aarons-MBP.lan>
2023-11-09 13:26:56 -0600

    added test for req e

--- tests/test_requirements_from_part_2.py
+++ tests/test_requirements_from_part_2.py
@@ -4,6 +4,8 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 @pytest.mark.skip
 def test_clicks_on_buttons():
@@ -56,6 +58,7 @@ def test_can_type_nums_onto_grids():
         driver.find_element(by=By.ID, value=f"right_{i}").send_keys(str(i))
 
 
+@pytest.mark.skip
 def test_can_get_list_of_weights():
     """
         Requirement d, test that we can get a list of weights
@@ -83,3 +86,19 @@ def test_can_get_list_of_weights():
     # search for the ordered list in game info
     driver.find_element(by=By.CLASS_NAME, value="game-info").find_elements(by=By.TAG_NAME, value="ol")
 
+
+def test_gold_bar_alert_is_present():
+    """
+        Requirement e, check if clicking a gold bar sends an alert
+    """
+    
+    url = os.getenv("POETRY_CHALLENGE_URL")    
+    driver = webdriver.Chrome()
+    driver.get(url)
+    driver.maximize_window()
+
+    # click on the 0th gold bar
+    driver.find_element(by=By.ID, value="coin_0").click()
+    
+    # fail the test if an alert is not present
+    assert WebDriverWait(driver, 30).until(EC.alert_is_present())
